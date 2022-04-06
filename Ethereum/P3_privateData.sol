02729124329e4013c9e94f2f555ca0c7ee427984
Paras Pidurkar <45849288+ParasPidurkar@users.noreply.github.com>
2022-04-06 16:18:34 +0530

    private data in solidity contract

---
+++ Ethereum/P3_privateData.sol
@@ -0,0 +1,24 @@
+//pragma solidity >=0.7.0 <0.9.0;
+
+contract Player{
+    string private player_name;
+    int private player_age;
+
+    function Player(string initialName,int initialAge) public{
+        player_name = initialName;
+        player_age = initialAge;
+    }
+
+    function setPlayerName(string newName) public{
+        player_name = newName;
+    }
+
+    function getPlayerName() public view returns(string){  //syntax function function_name function_type return_type 
+    // view is same as constant i.e function returns but does not modify data
+        return player_name;
+    }
+    function getPlayerage() public view returns(int){  
+        return player_age;
+    }
+}
+
